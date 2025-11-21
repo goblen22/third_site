453801aab33f95ca6729d2036a034d4bdc3f5841
goblen22 <lebedevaleksandr023@gmail.com>
2025-11-21 18:53:14 +0300

    fastapi_site

---
+++ router.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from repository import TaskRepository
+from shemas import STask, STaskAdd, STaskId
+
+router = APIRouter(
+    prefix="/tasks",
+    tags=['Tasks']
+)
+
+@router.post("")
+async def add_task(
+    task: Annotated[STaskAdd, Depends()],
+) -> STaskId:
+    task_id = await TaskRepository.add_one(task)
+    return {'ok':True, "task_id": task_id}
+
+@router.get("")
+async def get_tasks() -> list[STask]:
+    tasks = await TaskRepository.find_all()
+    return {"data": tasks}
