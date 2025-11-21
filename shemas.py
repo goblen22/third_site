9eee1287226690247e5eb70f6ec8d011e5f157b9
goblen22 <lebedevaleksandr023@gmail.com>
2025-11-21 19:22:56 +0300

    Add README and update shemas

--- shemas.py
+++ shemas.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class STaskAdd(BaseModel):
     name: str
@@ -7,6 +7,8 @@ class STaskAdd(BaseModel):
 
 class STask(STaskAdd):
     id: int
+
+    model_config = ConfigDict(from_attributes=True)
 
 class STaskId(BaseModel):
     ok: bool = True
